69bdfb739b6954ad028b127447522ae41a4c18c4
Rayizan <66122483+Rayizan@users.noreply.github.com>
2020-11-05 14:16:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/vernesong/OpenClash/releases/download/v0.40.15-beta
