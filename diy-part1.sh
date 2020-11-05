02d4e83bbb096386304cc608a57ed458656286c7
Rayizan <66122483+Rayizan@users.noreply.github.com>
2020-11-05 10:14:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/vernesong/OpenClash/releases/download/v0.40.15-beta/luci-app-openclash_0.40.15-beta_all.ipk
