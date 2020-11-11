3dcda332dbee88cf00cd3d38ce5f49e4763a885c
Rayizan <66122483+Rayizan@users.noreply.github.com>
2020-11-11 10:33:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # git clone https://github.com/vernesong/OpenClash.git
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 sed -i '$a src-git libcap-bin https://downloads.openwrt.org/snapshots/packages/arm_cortex-a15_neon-vfpv4/packages/packages.gz' feeds.conf.default
-sed -i '$a src-git libcap https://downloads.openwrt.org/snapshots/packages/arm_cortex-a15_neon-vfpv4/packages/packages' feeds.conf.default
