b51dba49be8cd4f67da957c4550061747711ff42
Rayizan <66122483+Rayizan@users.noreply.github.com>
2021-01-13 21:03:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+rm target/linux/ipq806x/files-4.14/arch/arm/boot/dts/qcom-ipq8064-r7500.dts
+cp files/qcom-ipq8064-r7500.dts target/linux/ipq806x/files-4.14/arch/arm/boot/dts
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
